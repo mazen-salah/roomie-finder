a8e15054f7619d1c016e6b7589962d5493dda90b
mazen-salah <mazentamer3056@gmail.com>
2024-09-04 06:36:40 +0300

    refactor: Update RFConformationDialog to accept a dynamic message parameter

--- lib/components/RFConformationDialog.dart
+++ lib/components/RFConformationDialog.dart
@@ -1,8 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:nb_utils/nb_utils.dart';
+
 import 'package:roomie_finder/utils/RFColors.dart';
 
 class RFConformationDialog extends StatelessWidget {
+  String message;
+  RFConformationDialog({
+    Key? key,
+    required this.message,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Align(
@@ -24,7 +31,7 @@ class RFConformationDialog extends StatelessWidget {
               child: Icon(Icons.done, color: Colors.white, size: 14),
             ),
             16.width,
-            Text('Registered Successfully!',
+            Text(message,
                 style: boldTextStyle(color: greenColor.withOpacity(0.7))),
           ],
         ).onTap(() {
