f7130a9d3519f90556e003cada2a5e26872939fe
Mazen Tamer Salah <mazentamer3056@gmail.com>
2024-09-15 06:58:39 +0300

    refactor: Improve sign-up flow by handling sign-in response and displaying messages

--- lib/views/Auth/RFSignUpScreen.dart
+++ lib/views/Auth/RFSignUpScreen.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:roomie_finder/components/RFCommonAppComponent.dart';
 import 'package:roomie_finder/controllers/RFAuthController.dart';
-import 'package:roomie_finder/main.dart';
 import 'package:roomie_finder/models/UserModel.dart';
 import 'package:roomie_finder/views/Auth/RFQuestionnaire.dart';
-import 'package:roomie_finder/views/Auth/RFSignInScreen.dart';
 import 'package:roomie_finder/utils/RFColors.dart';
 import 'package:roomie_finder/utils/RFWidget.dart';
 import 'package:roomie_finder/views/RFHomeScreen.dart';
@@ -322,18 +320,19 @@ class _RFSignUpScreenState extends State<RFSignUpScreen> {
             _isLoading = false;
           });
 
-            if (response['success']) {
+          if (response['success']) {
             _showSnackBar(context, response['message']);
-            await RFAuthController().signIn(emailController.text, passwordController.text);
-
+            var response2 = await RFAuthController()
+                .signIn(emailController.text, passwordController.text);
+            _showSnackBar(context, response2['message']);
             if (role == "tenant") {
               const QuestionnaireScreen().launch(context, isNewTask: true);
             } else if (role == "lessor") {
               const RFHomeScreen().launch(context);
             }
-            } else {
+          } else {
             _showSnackBar(context, response['message'], isError: true);
-            }
+          }
         }
       },
       child: Text('Create Account', style: boldTextStyle(color: white)),
