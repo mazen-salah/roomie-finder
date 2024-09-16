3444ea8fb92dcd60e531f330db4f7bfbfccda4a9
Mazen Tamer Salah <mazentamer3056@gmail.com>
2024-09-16 16:53:52 +0300

    feat: Add flutter_rating_bar dependency for enhanced rating functionality

--- lib/components/RFHotelDetailComponent.dart
+++ lib/components/RFHotelDetailComponent.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:roomie_finder/main.dart';
 import 'package:roomie_finder/models/RoomModel.dart';
@@ -19,10 +21,11 @@ class RFHotelDetailComponent extends StatefulWidget {
 
 class _RFHotelDetailComponentState extends State<RFHotelDetailComponent> {
   UserModel? ownerData;
-  bool isLoadingOwner = true; // Add a loading flag for owner data
+  bool isLoadingOwner = true;
+  double? userReview;
 
   @override
-  initState() {
+  void initState() {
     super.initState();
     fetchUserData();
   }
@@ -32,14 +35,30 @@ class _RFHotelDetailComponentState extends State<RFHotelDetailComponent> {
       UserModel? fetchedOwnerData = await RFAuthController()
           .fetchUserDataFromFirestore(widget.hotelData!.owner!);
 
+      // Fetch the current user ID
+      String userId =
+          'currentUserId'; // Replace with actual method to get current user ID
+
+      // Fetch the user's review if it exists
+      final reviewQuery = await FirebaseFirestore.instance
+          .collection('reviews')
+          .where('roomid', isEqualTo: widget.hotelData!.id)
+          .where('uid', isEqualTo: userId)
+          .get();
+
+      double? fetchedUserReview;
+      if (reviewQuery.docs.isNotEmpty) {
+        fetchedUserReview = reviewQuery.docs.first['review'];
+      }
+
       if (mounted) {
         setState(() {
           ownerData = fetchedOwnerData;
+          userReview = fetchedUserReview;
           isLoadingOwner = false;
         });
       }
     } catch (e) {
-      // Handle the error and stop loading
       setState(() {
         isLoadingOwner = false;
       });
@@ -47,96 +66,179 @@ class _RFHotelDetailComponentState extends State<RFHotelDetailComponent> {
     }
   }
 
+  Future<void> addReview(double rating) async {
+    if (widget.hotelData == null) return;
+
+    // Fetch the current user ID
+    String userId =
+        'currentUserId'; // Replace with actual method to get current user ID
+
+    try {
+      // Check if the user has already reviewed this room
+      final reviewQuery = await FirebaseFirestore.instance
+          .collection('reviews')
+          .where('roomid', isEqualTo: widget.hotelData!.id)
+          .where('uid', isEqualTo: userId)
+          .get();
+
+      if (reviewQuery.docs.isNotEmpty) {
+        toast('You have already reviewed this room');
+        return;
+      }
+
+      // If the user has not reviewed the room, add the review
+      List<double> reviews = widget.hotelData!.reviews ?? [];
+      reviews.add(rating);
+
+      await FirebaseFirestore.instance
+          .collection('rooms')
+          .doc(widget.hotelData!.id)
+          .update({'reviews': reviews});
+
+      // Add review to the reviews collection
+      await FirebaseFirestore.instance.collection('reviews').add({
+        'roomid': widget.hotelData!.id,
+        'uid': userId,
+        'review': rating,
+      });
+
+      setState(() {
+        widget.hotelData!.reviews = reviews;
+        userReview = rating;
+      });
+
+      toast('Review added successfully');
+    } catch (e) {
+      toast('Failed to add review');
+    }
+  }
+
+  double calculateAverageReview(List<double>? reviews) {
+    if (reviews == null || reviews.isEmpty) return 0.0;
+    return reviews.reduce((a, b) => a + b) / reviews.length;
+  }
+
+  Widget buildReviewSection() {
+    double averageReview = calculateAverageReview(widget.hotelData!.reviews);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Reviews', style: boldTextStyle(size: 18)),
+        const SizedBox(height: 8),
+        averageReview == 0.0
+            ? Text('No reviews yet', style: secondaryTextStyle(size: 14))
+            : Row(
+                children: [
+                  Text(averageReview.toStringAsFixed(1),
+                      style: boldTextStyle(size: 16)),
+                  const SizedBox(width: 8),
+                  const Icon(Icons.star, color: Colors.amber, size: 16),
+                ],
+              ),
+        const SizedBox(height: 16),
+        RatingBar.builder(
+          initialRating: userReview ?? 0,
+          minRating: 1,
+          direction: Axis.horizontal,
+          allowHalfRating: true,
+          itemCount: 5,
+          itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+          itemBuilder: (context, _) =>
+              const Icon(Icons.star, color: Colors.amber),
+          onRatingUpdate: (rating) => addReview(rating),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 24),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Owner Info Section
           isLoadingOwner
               ? const Center(child: CircularProgressIndicator())
-              : ownerData != null
-                  ? buildOwnerInfo(context)
-                  : const Text('Owner data could not be loaded'),
-          24.height,
-          // Property Info Section
-          buildPropertyInfo(context),
-          24.height,
-          // Description Section
+              : buildOwnerInfo(),
+          const SizedBox(height: 24),
+          buildPropertyInfo(),
+          const SizedBox(height: 24),
           buildDescriptionSection(),
-          24.height,
-          // Facilities Section
+          const SizedBox(height: 24),
           buildFacilitiesSection(),
         ],
       ),
     );
   }
 
-  Widget buildOwnerInfo(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: context.cardColor,
-        boxShadow: defaultBoxShadow(),
-      ),
-      child: Row(
-        children: [
-          rfCommonCachedNetworkImage(ownerData!.profileImageUrl.validate(),
-                  width: 70, height: 70, fit: BoxFit.cover)
-              .cornerRadiusWithClipRRect(35),
-          16.width,
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(ownerData!.fullName.validate(),
-                  style: boldTextStyle(size: 18)),
-              4.height,
-              Text('Lessor', style: secondaryTextStyle(size: 14)),
-            ],
-          ).expand(),
-          16.width,
-          Column(
-            children: [
-              AppButton(
-                onTap: () {
-                  launchCall(ownerData!.phone.validate());
-                },
-                color: rfPrimaryColor,
-                elevation: 0,
-                width: 40,
-                height: 40,
-                shapeBorder: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: rfCall.iconImage(iconColor: white, size: 18),
-              ),
-              8.height,
-              AppButton(
-                onTap: () {
-                  launchMail(ownerData!.email.validate());
-                },
-                color: rfPrimaryColor,
-                elevation: 0,
-                width: 40,
-                height: 40,
-                shapeBorder: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: rfMessage.iconImage(iconColor: white, size: 18),
-              ),
-            ],
-          ),
-        ],
-      ),
+  Widget buildOwnerInfo() {
+    return ownerData == null
+        ? const Text('Owner data could not be loaded')
+        : Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: context.cardColor,
+              boxShadow: defaultBoxShadow(),
+            ),
+            child: Row(
+              children: [
+                rfCommonCachedNetworkImage(
+                        ownerData!.profileImageUrl.validate(),
+                        width: 70,
+                        height: 70,
+                        fit: BoxFit.cover)
+                    .cornerRadiusWithClipRRect(35),
+                const SizedBox(width: 8),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(ownerData!.fullName.validate(),
+                        style: boldTextStyle(size: 18)),
+                    const SizedBox(height: 4),
+                    Text('Lessor', style: secondaryTextStyle(size: 14)),
+                  ],
+                ).expand(),
+                const SizedBox(width: 16),
+                buildOwnerActionButtons(),
+              ],
+            ),
+          );
+  }
+
+  Widget buildOwnerActionButtons() {
+    return Column(
+      children: [
+        AppButton(
+          onTap: () => launchCall(ownerData!.phone.validate()),
+          color: rfPrimaryColor,
+          elevation: 0,
+          width: 40,
+          height: 40,
+          shapeBorder:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          child: rfCall.iconImage(iconColor: white, size: 18),
+        ),
+        const SizedBox(height: 8),
+        AppButton(
+          onTap: () => launchMail(ownerData!.email.validate()),
+          color: rfPrimaryColor,
+          elevation: 0,
+          width: 40,
+          height: 40,
+          shapeBorder:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          child: rfMessage.iconImage(iconColor: white, size: 18),
+        ),
+      ],
     );
   }
 
-  Widget buildPropertyInfo(BuildContext context) {
+  Widget buildPropertyInfo() {
     return Container(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(8),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
         color: context.cardColor,
@@ -148,24 +250,16 @@ class _RFHotelDetailComponentState extends State<RFHotelDetailComponent> {
           Row(
             children: [
               const Icon(Icons.location_on, color: rfPrimaryColor),
-              8.width,
+              const SizedBox(width: 8),
               Text(widget.hotelData!.address.validate(),
                   style: boldTextStyle(size: 16)),
             ],
           ),
-          8.height,
+          const SizedBox(height: 8),
           Text('${widget.hotelData!.location}, Saudi Arabia',
               style: primaryTextStyle(size: 14)),
-          16.height,
-          Row(
-            children: [
-              Text('Reviews: ',
-                  style: primaryTextStyle(
-                      color: appStore.isDarkModeOn ? white : rfTextColor)),
-              Text(widget.hotelData!.reviews.validate().toString(),
-                  style: boldTextStyle(size: 16)),
-            ],
-          ),
+          const SizedBox(height: 16),
+          buildReviewSection(),
         ],
       ),
     );
@@ -173,7 +267,7 @@ class _RFHotelDetailComponentState extends State<RFHotelDetailComponent> {
 
   Widget buildDescriptionSection() {
     return Container(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(8),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
         color: context.cardColor,
@@ -183,7 +277,7 @@ class _RFHotelDetailComponentState extends State<RFHotelDetailComponent> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('Description', style: boldTextStyle(size: 18)),
-          8.height,
+          const SizedBox(height: 8),
           Text(widget.hotelData!.description.validate(),
               style: secondaryTextStyle(size: 14)),
         ],
@@ -193,7 +287,7 @@ class _RFHotelDetailComponentState extends State<RFHotelDetailComponent> {
 
   Widget buildFacilitiesSection() {
     return Container(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(8),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
         color: context.cardColor,
@@ -203,7 +297,7 @@ class _RFHotelDetailComponentState extends State<RFHotelDetailComponent> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('Facilities', style: boldTextStyle(size: 18)),
-          16.height,
+          const SizedBox(height: 16),
           buildFacilitiesList(widget.hotelData!.facilities),
         ],
       ),
@@ -224,9 +318,7 @@ class _RFHotelDetailComponentState extends State<RFHotelDetailComponent> {
       avatar: const Icon(Icons.done, size: 16, color: rfPrimaryColor),
       label: Text(facility, style: secondaryTextStyle(size: 14)),
       backgroundColor: rfPrimaryColor.withOpacity(0.1),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
     );
   }
 }
